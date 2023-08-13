d9e0f27c4e3a087a7868a39febfb93815acf8269
Nadiia17 <nababiy@gmail.com>
2023-08-13 16:49:47 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+const PROMPT_DELAY = 1000;
+let timerId = null;
+stopBtnEl.disabled = true;
 
+startBtnEl.addEventListener('click', onStartClickHandler);
+function onStartClickHandler() {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, PROMPT_DELAY);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtnEl.addEventListener('click', onStopClickHandler);
+function onStopClickHandler() {
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+  clearInterval(timerId);
+}
